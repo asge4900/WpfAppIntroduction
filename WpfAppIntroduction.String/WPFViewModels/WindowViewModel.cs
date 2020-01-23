cb13c44ea29278402802611b7268921b1188f95d
Asger <asge4900@edu.campusvejle.dk>
2020-01-23 15:09:15 +0100

    Add

--- WpfAppIntroduction.String/WPFViewModels/WindowViewModel.cs
+++ WpfAppIntroduction.String/WPFViewModels/WindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media;
@@ -16,15 +17,20 @@ namespace WpfAppIntroduction.String
         {
             CountAllLinesCommand = new RelayCommand(CountAllLines);            
             CountAllVowelsCommand = new RelayCommand(CountAllVowels);
+            RemoveAllVowelsCommand = new RelayCommand(RemoveAllVowels);
+
+            TextBoxMain = GetTestString();
         }
 
         #endregion
 
         #region Properties
 
-        public string TextBox { get; set; }
+        public string TextBoxMain { get; set; }
 
-        public string TextBox2 { get; set; }
+        public string TextBoxResult { get; set; }
+
+        public string SearchText { get; set; }
 
         #endregion
 
@@ -35,49 +41,77 @@ namespace WpfAppIntroduction.String
 
         public ICommand CountAllVowelsCommand { get; set; }
 
+        public ICommand RemoveAllVowelsCommand { get; set; }
+
+        public ICommand MarkAndCountWordCommand { get; set; }
+
         #endregion
 
 
-        #region Methods
-
-        private string TextFile()
-        {
-            return "Hej";
-        }
+        #region Command Methods       
 
         private void CountAllLines()
-        {            
+        {
+            //var lineCount = File.ReadLines(@"..\Admiralen.txt").Count(line => !line.All(char.IsWhiteSpace));
+            
+            // Remove empty line
+            var resultString = Regex.Replace(TextBoxMain, @"^\s+$[\r\n]*", string.Empty, RegexOptions.Multiline);
 
-            var lineCount = File.ReadLines(@"..\Admiralen.txt").Count(line => !line.All(char.IsWhiteSpace));            
-
-            using (var reader = File.OpenText(@"..\Admiralen.txt"))
-            { 
-                TextBox2 = reader.ReadToEnd();
-            }
-
-            TextBox = lineCount.ToString();            
+            // Count lines
+            var lineCount = resultString.Split('\n').Length;
+            
+            TextBoxResult = lineCount.ToString();            
         }
 
         private void CountAllVowels()
         {
             // Build a list of vowels up front:
-            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u', 'æ', 'ø' };
+            var vowels = new HashSet<char> { 'a', 'e', 'i', 'o', 'u', 'y', 'æ', 'ø', 'å' };          
 
-            int total = 0;                          
+            int total = TextBoxMain.ToLower().Count(c => vowels.Contains(c));                               
+
+            TextBoxResult = total.ToString();
+        }
+
+        private void RemoveAllVowels()
+        {
+            // Build a list of vowels up front:
+            var vowels = new HashSet<char> {'A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'Y', 'y', 'Æ', 'æ', 'Ø', 'ø', 'Å', 'å' }; 
+
+            string text = new string(TextBoxMain.Where(c => !vowels.Contains(c)).ToArray());
+
+            TextBoxResult = text;
+        }
+
+        private void MarkAndCountWord()
+        {
+            
+            int count = 0;
+            string textToSearch = TextBoxMain;
+            int i = textToSearch.IndexOf(SearchText);
+            while (i != -1)
+            {
+                textToSearch = textToSearch.Insert(i, "#>");
+                count++;
+                i = textToSearch.IndexOf(SearchText, i + 3);
+            }
+            textToSearch = $"Ordet {SearchText} blev fundet {count} gange og er blevet markeret med #>\n\n{textToSearch}";            
+        }
+
+        #endregion
+
+        #region Methods
+
+        private string GetTestString()
+        {
+            string s = string.Empty;
 
             using (var reader = File.OpenText(@"..\Admiralen.txt"))
             {
-                string test = reader.ReadToEnd();
-
-                total = test.Count(c => vowels.Contains(c));
+                s = reader.ReadToEnd();
             }
 
-            using (var reader = File.OpenText(@"..\Admiralen.txt"))
-            {
-                TextBox2 = reader.ReadToEnd();
-            }
-
-            TextBox = total.ToString();
+            return s;
         }
 
         #endregion
